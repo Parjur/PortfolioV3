2e5446dd465a2c24d03478e697fc5fb2c01c5771
Tristan Fern <tristan.fern@gmail.com>
2023-07-28 16:35:00 +0200

    update projects

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Welcome from './components/typewriter/index.jsx';
 import Projects from './components/projects/index.jsx';
 import Skills from './components/skills/index.jsx';
 import Contact from './components/contact/index.jsx';
-//import Footer from './components/footer/index.jsx';
 
 function App() {
   return (
